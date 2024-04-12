37a849c58be1ae17ace4c5a12b623bf25456c389
Amanda Breidenbach <abreidenbach@hotmail.com>
2024-04-12 11:36:34 -0300

    rotasreach

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Menu from "./Menu";
+function Home() {
+    return(
+        <div>
+            <h1>Home</h1>
+            <p>Nesse projeto aprendemos sistema de roteamento.</p>
+            <Menu />
+        </div>
+    )
+}
+export default Home
